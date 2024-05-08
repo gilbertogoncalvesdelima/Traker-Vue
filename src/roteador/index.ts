5c34bf2cf7aadbb4aa188aeeeac05e55f43ee860
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2024-05-08 15:12:16 -0300

    Extraindo a view

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -2,6 +2,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from "../views/Projetos/Formulario.vue";
 
 
 // Define o tipo de registro de rota usando RouteRecordRaw.
@@ -17,7 +18,19 @@ const rotas: RouteRecordRaw[] = [
         path: '/projetos',
         name: 'Projetos',
         component: Projetos
-    },         
+    },
+    {
+        // Rota raiz, exibindo a lista de tarefas.
+        path: '/projetos/novo',
+        name: 'Novo projeto',
+        component: Formulario
+    }, 
+    {
+        // Rota raiz, exibindo a lista de tarefas.
+        path: '/projetos/:id',
+        name: 'Editar projeto',
+        component: Formulario
+    },          
 ]
 
 // Cria uma instância de roteador usando o modo de histórico hash e as rotas definidas.
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,59 +1,46 @@
 <template>
     <section class="projetos">
         <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
-                <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjet">
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>
+        <router-link to="/projetos/novo" class="button">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo projeto</span>
+        </router-link>
         <table class="table is-fullwidth">
-          <thead>
-            <tr>
-                <th>
-                    ID
-                </th>
-                <th>
-                    Nome
-                </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="projeto in projetos" :key="projeto.id">
-                <td>{{ projeto.id }}</td>
-                <td>{{ projeto.nome }}</td>
-            </tr>
-          </tbody>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                    <td>
+                        <router-link :to="`/projetos/${projeto.id}`" class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </span>
+                        </router-link>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </section>
 </template>
 
-
 <script lang="ts">
-import { useStore } from "@/store"
-import { computed, defineComponent } from 'vue';
+import { useStore } from "@/store";
+import { computed, defineComponent } from "vue";
+
 export default defineComponent({
-    name: 'Projetos',
-    data() {
-        return {
-            nomeDoProjeto: '',
-        }
-    },
-    methods: {
-        salvar () {
-         this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-         this.nomeDoProjeto = ""; 
-        },
-    },
-    setup () {
+    name: "Projetos",
+    setup() {
         const store = useStore()
         return {
-            store,
             projetos: computed(() => store.state.projetos)
         }
     }
@@ -62,6 +49,6 @@ export default defineComponent({
 
 <style scoped>
 .projetos {
-    padding: 1.25ren;
+    padding: 1.25rem;
 }
 </style>
